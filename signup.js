6515a695a40088905517e0c238816d7d359a1d24
Ajay4-hub <akajaykote@gmail.com>
2024-09-05 17:23:31 +0530

    Delete signup.js

--- signup.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("signupForm").addEventListener("submit", function (event) {
-    event.preventDefault();
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    console.log("Registering user:", email, password);
-    window.location.href = "signup-success.html"; // Redirect to the success page
-  });
-  
